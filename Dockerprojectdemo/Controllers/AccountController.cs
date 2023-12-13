ec7201abea2278596ddf0cd99674a2b43b200320
drushikachanda <chandadrushika6@gmail.com>
2023-12-13 17:58:06 +0530

    Add project files.

---
+++ Dockerprojectdemo/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using Dockerprojectdemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dockerprojectdemo.Controllers
+{
+    public class AccountController : Controller
+    {
+        // Simulated user data (replace with your actual authentication logic)
+        private readonly User _user = new User
+        {
+            Id = 1,
+            Username = "maximus",
+            Password = "perseus"
+        };
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(User model)
+        {
+            if (ModelState.IsValid && model.Username == _user.Username && model.Password
+           == _user.Password)
+            {
+                // Successful login, redirect to dashboard
+                return RedirectToAction("Index", "Dashboard");
+            }
+            // Invalid login, show error message
+            ModelState.AddModelError("", "Invalid username or password");
+            return View(model);
+        }
+    }
+}
